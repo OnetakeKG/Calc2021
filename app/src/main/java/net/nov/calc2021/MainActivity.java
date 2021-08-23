61f25a4febd96a140fca8a5cec5cd8c8be1c2843
OnetakeKG <86379850+OnetakeKG@users.noreply.github.com>
2021-08-23 16:15:54 +0300

    Calc_and_logic_for_keys

--- app/src/main/java/net/nov/calc2021/MainActivity.java
+++ app/src/main/java/net/nov/calc2021/MainActivity.java
@@ -3,12 +3,105 @@ package net.nov.calc2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<text> extends AppCompatActivity {
+    Button add, mul, div, sub, equal, degree, percent, clear;
+    double ans = 0;
+    double arg1, arg2; // в будущем - аргументы введенные пользователем в EditText
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add = findViewById(R.id.key_add);
+        mul = findViewById(R.id.key_mul);
+        div = findViewById(R.id.key_div);
+        sub = findViewById(R.id.key_sub);
+        degree = findViewById(R.id.key_degree);
+        percent = findViewById(R.id.key_percent);
+
+        equal = findViewById(R.id.key_equal);
+        clear = findViewById(R.id.key_clear);
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = arg1;
+                double b = arg2;
+                ans = a + b;
+            }
+        });
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = arg1;
+                double b = arg2;
+                ans = a - b;
+            }
+        });
+
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = arg1;
+                double b = arg2;
+                ans = a * b;
+            }
+        });
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = arg1;
+                double b = arg2;
+                ans = a / b;
+            }
+        });
+
+        degree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = arg1;
+                double b = arg2;
+                double result = 1.0;
+                for (int i = 1; i <= arg2; i++) {
+                    result = result * arg1;
+                    ans = result;
+                }
+            }
+        });
+
+        percent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = arg1;
+                double b = arg2;
+                ans = a * b / 100;
+            }
+        });
+
+
+
+        equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
     }
+
 }
