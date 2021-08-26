e6d95ce39cca385ee00f803afec4c464972a1aed
OnetakeKG <86379850+OnetakeKG@users.noreply.github.com>
2021-08-26 14:33:24 +0300

    Calc_resources

--- app/src/main/java/net/nov/calc2021/MainActivity.java
+++ app/src/main/java/net/nov/calc2021/MainActivity.java
@@ -6,8 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity<text> extends AppCompatActivity {
-    Button add, mul, div, sub, equal, degree, percent, clear;
+public class MainActivity extends AppCompatActivity {
+    private Button mul;
+    private Button div;
+    private Button sub;
+    private Button equal;
+    private Button degree;
+    private Button percent;
+    private Button clear;
     double ans = 0;
     double arg1, arg2; // в будущем - аргументы введенные пользователем в EditText
 
@@ -16,7 +22,7 @@ public class MainActivity<text> extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        add = findViewById(R.id.key_add);
+        Button add = findViewById(R.id.key_add);
         mul = findViewById(R.id.key_mul);
         div = findViewById(R.id.key_div);
         sub = findViewById(R.id.key_sub);
